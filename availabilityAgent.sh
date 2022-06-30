0e05bafb69fad48bd8251b962f00ea77087d9f2b
refaelsa <refael32901@gmail.com>
2022-06-30 16:15:35 +0300

    After section 3

---
+++ availabilityAgent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TEST_PERIDOCITY=5
+
+while true
+do
+while read line; do
+        ping -D $line -c  1 1>/dev/null
+        EC=$?
+                if [  "$EC" -eq "0" ]; then
+                echo Test result for ${line} is 1 at $(date +%s%N)
+                else
+                echo Test result for ${line} is 0 at $(date +%s%N)
+                fi
+done < hosts
+echo
